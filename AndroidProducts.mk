166689c6803b8bdd964cf8567108d865a23d529c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-12-22 14:20:28 +0100

    itel-S662LCN: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: itel Model: itel S662LCN Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_itel-S662LCN.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_itel-S662LCN-user \
+    omni_itel-S662LCN-userdebug \
+    omni_itel-S662LCN-eng
